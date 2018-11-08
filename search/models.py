1f31c98475105b3f7b1c13f61c02c7f89f812fa3
YIzhuo_Li <lyz_hust@qq.com>
2018-11-07 21:41:58 -0600

    associate

---
+++ search/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+from elasticsearch_dsl import Document, Keyword, Text, Completion
+
+from elasticsearch_dsl.analysis import CustomAnalyzer as _CustomAnalyzer
+
+from elasticsearch_dsl.connections import connections
+# Define a default Elasticsearch client
+connections.create_connection(hosts=['localhost'])
+
+class CustomAnalyzer(_CustomAnalyzer):
+    def get_analysis_definition(self):
+        return{}
+
+ik_analyzer = CustomAnalyzer("ik_max_word", filter=["lowercase"])
+
+class IndeedType(Document):
+    #完成搜索建议
+    suggest = Completion(analyzer=ik_analyzer)#理论上可以这样
+    #要保存在elasticsearch中的数据类型
+    job_title = Text(analyzer="ik_max_word")
+    job_location = Text(analyzer="ik_max_word")
+    job_summary = Text(analyzer="ik_max_word")
+    job_salary = Text(analyzer="ik_max_word")
+    company_name = Text(analyzer="ik_max_word")
+    job_href = Text(analyzer="ik_max_word")
+    job_star = Text(analyzer="ik_max_word")
+    job_review = Text(analyzer="ik_max_word")
+
+
+    class Meta:
+        index = "Indeed"
+        doc_type = "data_science"
+
+    class Index:
+        name = "indeed"
+        doc_type = "data_science"
+
+    # Display cluster health
+    print(connections.get_connection().cluster.health())
+
+
+if __name__ == "__main__":
+    IndeedType.init()
---
+++ search/views.py
@@ -0,0 +1,36 @@
+import pickle
+from django.shortcuts import render
+import json
+
+from django.utils.datastructures import OrderedSet
+from django.views.generic.base import View
+from search.models import IndeedType
+from django.http import HttpResponse
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from django.views.generic.base import RedirectView
+
+# Create your views here.
+class SearchSuggest(View):
+    """搜索建议"""
+    def get(self, request):
+        key_words = request.GET.get('s', '')
+        # current_type = request.GET.get('s_type', '')
+        # if current_type == "title":
+        re_data = []
+        if key_words:
+            s = IndeedType.search()
+            """fuzzy模糊搜索, fuzziness 编辑距离, prefix_length前面不变化的前缀长度"""
+            s = s.suggest('my_suggest', key_words, completion={
+                "field": "suggest", "fuzzy": {
+                    "fuzziness": 2
+                },
+                "size": 10
+            })
+            suggestions = s.execute()
+            for match in suggestions.suggest.my_suggest[0].options:
+                source = match._source
+                re_data.append(source["job_title"])
+        return HttpResponse(json.dumps(re_data), content_type="application/json")
+        # else:
+        #     pass
